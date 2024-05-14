a56f602f69b39239ef5b1c2a3a6e0113f33f6f74
dutabrahmana <dutalinggau25@gmail.com>
2024-05-14 10:29:24 +0700

    menyelesaikan int main

--- Prakpemdas1/Prakpemdas1.cpp
+++ Prakpemdas1/Prakpemdas1.cpp
@@ -17,5 +17,34 @@ public:
 	}
 };
 
-int Mahasiswa::nim = 100;
+int Mahasiswa::nim = 191;
+
+void Mahasiswa::setID() {
+	id = ++nim;
+}
+
+void Mahasiswa::printAll()
+{
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main()
+{
+	Mahasiswa mhs1("Lia Kurnia");
+	Mahasiswa mhs2("Asroni");
+	Mahasiswa mhs3("Andi Kurniawan");
+	Mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	system("pause");
+	return 0;
+
+
+}
 
