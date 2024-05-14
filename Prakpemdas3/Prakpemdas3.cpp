e2bb2132dafe748250d33c97890d2b1913f09745
dutabrahmana <dutalinggau25@gmail.com>
2024-05-14 11:36:11 +0700

    menyelesaikan projeck

--- Prakpemdas3/Prakpemdas3.cpp
+++ Prakpemdas3/Prakpemdas3.cpp
@@ -7,21 +7,22 @@ private:
     int nim;
     string nama;
 public:
-    mahasiswa();
-    mahasiswa(int);
+    mahasiswa() {
+        nim = 0;
+        nama = "";
+    };
+    mahasiswa(int iNim) {
+        nim = iNim;
+    };
     mahasiswa(string);
     mahasiswa(int iNim, string iNama);
     void cetak();
 };
 
-mahasiswa::mahasiswa() {
+mahasiswa::mahasiswa(string iNama) {
     nim = 0;
-    nama = "";
-}
-
-mahasiswa::mahasiswa(int iNim) {
-    nim = iNim;
-}
+    nama = iNama;
+};
 
 mahasiswa::mahasiswa(int iNim, string iNama) {
     nim = iNim;
@@ -29,10 +30,25 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 }
 
 void mahasiswa::cetak() {
-    cout
+    cout << endl << "Nim =" << nim << endl;
+    cout << "Nama = " << nama << endl;
 }
 
 int main()
 {
+    mahasiswa ariel;
+    mahasiswa dawa = 172;
+    mahasiswa faiq("Bjong");
+    mahasiswa gibran (191, "Duta");
     
+    ariel.cetak();
+    dawa.cetak();
+    faiq.cetak();
+    gibran.cetak();
+     
+    system("pause");
+
+    return 0;
+
+
 }
